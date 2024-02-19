da358d35b5b4a9cc226e1b46b6e667df7a64114c
baekchan1024 <baekchan1024@gmail.com>
2024-02-19 15:42:32 +0900

    2장 끝

--- chapter01/src/main/java/calculator/tutorial/StringCalculator.java
+++ chapter01/src/main/java/calculator/tutorial/StringCalculator.java
@@ -1,10 +1,95 @@
 package calculator.tutorial;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
 
+    // 구현 TDD 적용 X
+//    public int add(String input) {
+//        return Arrays.stream(input.split("[^0-9]")).filter(e -> !e.isEmpty()).mapToInt(Integer::parseInt).sum();
+//    }
+
+//    // 책 TDD 적용 X
+//    public int add(String input) {
+//        if (Objects.isNull(input)) {
+//            return 0;
+//        }
+//        if (input.isEmpty()) {
+//            return 0;
+//        }
+//
+//        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+//        List<String> stringList = new ArrayList<>();
+//        if (m.find()) {
+//            String customDelimeter = m.group(1);
+//            String[] tokens = m.group(2).split(customDelimeter);
+//            stringList = Arrays.stream(tokens).toList();
+//        }
+//        return stringList.stream().filter(e -> !e.isEmpty()).mapToInt(Integer::parseInt).sum();
+//    }
+
+    // 1차 TDD 작성
+    // 문제점 : 이미 작성된 구현 바디를 건들지말자.... return 대한 비즈니스 로직을 건들다보니 메서드의 변경이 일어나서 사이드 임펙트 가능성이 생긴다.
+//    public int add(String input) {
+//        if (Objects.isNull(input) || input.isEmpty()) {
+//            return 0;
+//        }
+//        return Arrays.stream(input.split(",|;")).mapToInt(Integer::parseInt).sum();
+//    }
+
     public int add(String input) {
-        return Arrays.stream(input.split("[^0-9]")).filter(e -> !e.isEmpty()).mapToInt(Integer::parseInt).sum();
+        if (this.isBlank(input)) {
+            return 0;
+        }
+        return sum(toInts(split(input)));
     }
+
+    private boolean isBlank(String input) {
+        return Objects.isNull(input) || input.isEmpty();
+    }
+
+    /*
+    * 구분자 기준으로 문자열 배열을 만드는 일
+    * */
+    private String[] split(String input) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(input);
+        if (m.find()) {
+            String custom = m.group(1);
+            return m.group(2).split(custom);
+        }
+        return input.split(",|:");
+    }
+
+    /*
+    * 문자열 배열 -> 숫자형 배열
+    * */
+    private int[] toInts(String[] values) {
+        return Arrays.stream(values).mapToInt(this::toInt).toArray();
+    }
+
+    /*
+    * 문자열 -> 숫자형
+    * */
+    private int toInt(String value) {
+        int i = Integer.parseInt(value);
+        if (i < 0) {
+            throw new RuntimeException();
+        }
+        return i;
+    }
+
+    /*
+    * 숫자형 배열의 합
+    * */
+    private int sum(int[] values) {
+        return Arrays.stream(values).sum();
+    }
+
+
+
 }
--- chapter01/src/test/java/calculator/tutorial/StringCalculatorTest.java
+++ chapter01/src/test/java/calculator/tutorial/StringCalculatorTest.java
@@ -1,10 +1,22 @@
 package calculator.tutorial;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/*
+ * 작은 요구사항 기준으로 유닛 테스트 진행할 것
+ * 1. 빈 문자열일 경우 -> 0
+ * 2. 숫자가 하나일 경우 -> 숫자
+ * 3. 쉼표 구분자가 있을 경우 -> 숫자의 합
+ * 4. 쉼표 또는 클론 구분자가 있을 경우 -> 숫자의 합
+ * 5. // 또는 \n 문자 사이의 커스텀 구분자 지정 -> 숫자의 합
+ * 6. 음수를 전달하는 경우 -> runtimeException
+ * */
 class StringCalculatorTest {
 
     private StringCalculator stringCalculator;
@@ -15,8 +27,46 @@ class StringCalculatorTest {
     }
 
     @Test
-    public void add() {
-        assertEquals(stringCalculator.add("\\;\n1;2;3"), 6);
+    @DisplayName("빈 문자열일 경우")
+    public void nullOrEmpty() {
+        assertEquals(stringCalculator.add(""), 0);
+        assertEquals(stringCalculator.add(null), 0);
     }
+
+    @Test
+    @DisplayName("숫자가 하나일 경우")
+    public void onlyNumber() {
+        assertEquals(stringCalculator.add("1"), 1);
+        assertEquals(stringCalculator.add("3"), 3);
+    }
+
+    @Test
+    @DisplayName("쉼표 구분자가 있을 경우")
+    public void oneDelimiter() {
+        assertEquals(stringCalculator.add("1,2"), 3);
+    }
+
+    @Test
+    @DisplayName("쉼표 또는 클론 구분자가 있을 경우")
+    public void twoDelimiter() {
+        assertEquals(stringCalculator.add("1,2:3"), 6);
+    }
+
+    @Test
+    @DisplayName("// 또는 \\n 문자 사이의 커스텀 구분자 지정")
+    public void customDelimiter() {
+        assertEquals(stringCalculator.add("//;\n1;2;3"), 6);
+    }
+
+    @Test
+    @DisplayName("음수를 전달하는 경우")
+    public void addNegative() {
+        assertThrows(RuntimeException.class, () -> stringCalculator.add("-1,2,3"));
+    }
+
+//    @Test
+//    public void add() {
+//        assertEquals(stringCalculator.add("//;\n1;2;3"), 6);
+//    }
 
 }
